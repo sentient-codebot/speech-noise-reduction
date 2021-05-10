d11cb24664a99b47d46e32021cf70f1af4d15de3
Nan Lin <n.lin-2@student.tudelft.nl>
2021-05-10 20:51:45 +0200

    Load data

---
+++ create_dataset.m
@@ -0,0 +1,34 @@
+% this script 
+%   load .wav files as vectors
+%   create noisy data (AWGN)
+%   save to target .mat file
+% 
+% load following files: 
+%   clean_1, 
+%   clean_2, 
+%   babble
+% create (AWGN) noisy signals:
+%   noisy_1,
+%   noisy_2
+% specified SNR(dB)
+%
+SNR = 10;
+
+filefolder = 'data\';
+filenames = {'clean_speech.wav'; 'clean_speech_2.wav'; 'babble_noise.wav'};
+datanames = {'clean_1'; 'clean_2'; 'babble'};
+for i = 1:3
+    filepath = strcat(filefolder,filenames{i});
+    importfile(filepath)
+    assignin('base', datanames{i}, data);
+    clear data filepath
+end
+
+noisy_1 = awgn(clean_1,SNR,'measured');
+noisy_2 = awgn(clean_2,SNR,'measured');
+
+
+save([filefolder,'data.mat'],'clean_1','clean_2','babble',...
+    'noisy_1','noisy_2','fs')
+
+clear 
