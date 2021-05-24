3891037bc42f620d7d09a25cd06cf1a533a39b98
Nan Lin <nanlin1997@outlook.com>
2021-05-25 00:12:45 +0200

    numerical modifications, avoided NaN now

--- main.m
+++ main.m
@@ -12,33 +12,39 @@ OVERLAP_RATIO = 0.5;
 output=[];
 frame_count = 0;
 sigma_N2 = 0.09*ones(320,1);
-SNR_priori = 10*ones(320,1);
+SNR_priori = 31.62*ones(320,1); % 15dB optimal according to literature, why?
 Sl = 0*ones(320,1); % how much?
+P_smooth = 0.5*ones(320,1);
+GLRs = [];
+sigma_NK2=[];
+P_H1_posts=[];
+K = 11;
 while next<length(noisy_1)
     %% frame
     frame_count = frame_count+1;
     [yl, next] = frame(noisy_1, next, "overlap_ratio", OVERLAP_RATIO);
     
     %% dft
-    Yl = fft(yl);
+    Yl = fft(yl.*hann(320));
     
     %% processing 1: noise PSD tracking
-    [sigma_N2,GLR] = noise_track(Yl,sigma_N2,....
-        'alpha',0.95,...
-        'P_H1',0.7,...
-        'SNR_H1',SNR_priori); % which alpha should be used?
+    [sigma_N2,GLR,P_smooth,P_H1_post] = noise_track(Yl,sigma_N2,P_smooth,....
+        'alpha',0.8,... % 0.8 according to literature? 0.97 works
+        'P_H1',0.8); % which alpha should be used?
 %     sigma_N2 = true_sigma_N2;
     %% processing 2: a priori SNR estimate (DD method)
-    alpha_DD = 0.98;
+    alpha_DD = 0.99;
     SNR_priori = alpha_DD*abs(Sl).^2./sigma_N2+...
-        (1-alpha_DD)*max(abs(Yl).^2./sigma_N2,0);
+        (1-alpha_DD)*max(abs(Yl).^2./sigma_N2-1,0);
 %     SNR_priori(K)
     %% processing 3: apply Gain function
     % Wiener Gain
     Sl = SNR_priori./(SNR_priori+1).*Yl; % SNR/(SNR+1) = P_SS/P_YY
     % MMSE Gain (Magnitude of S Rayleigh distributed)
 %     Sl = mmse_gain(GLR,SNR_priori,abs(Yl).^2./sigma_N2).*Yl;
-    
+    GLRs = [GLRs;GLR];
+    sigma_NK2=[sigma_NK2;sigma_N2(K)];
+    P_H1_posts=[P_H1_posts;P_H1_post(K)];
     %% idft
     sl = ifft(Sl);
     
@@ -49,12 +55,16 @@ end
 
 %% show result
 figure
-subplot(2,1,1)
-plot(output(1:length(clean_1)),'.')
+subplot(3,1,1)
+% plot(output(1:length(clean_1)),'.')
 hold on
 plot(clean_1)
-reduced_mse = norm(output(1:length(clean_1))-clean_1,2)
-subplot(2,1,2)
+% reduced_mse = norm(output(1:length(clean_1))-clean_1,2)
+subplot(3,1,2)
+plot(output(1:length(clean_1)))
+subplot(3,1,3)
 plot(noisy_1)
-noisy_mse = norm(noisy_1(1:length(clean_1))-clean_1,2)
+% noisy_mse = norm(noisy_1(1:length(clean_1))-clean_1,2) 
+% NOTE: it's not fair to compare the mse in time domain, because spectral
+% magnitude mse is what we truly care about. 
 
--- noise_track.m
+++ noise_track.m
@@ -1,4 +1,4 @@
-function [sigma_N2,GLR] = noise_track(Yl,sigma_N2_past,options)
+function [sigma_N2,GLR,P_smooth,P_H1_post] = noise_track(Yl,sigma_N2_past,P_smooth_past,options)
 % track noise PSD
 % method:
 %   MMSE based with SPP. Assuming N and S prior has complex Gaussian distribution
@@ -11,9 +11,10 @@ function [sigma_N2,GLR] = noise_track(Yl,sigma_N2_past,options)
     arguments
         Yl (:,1) 
         sigma_N2_past (:,1) {mustBePositive} 
-        options.alpha (1,1) = 0.96
-        options.SNR_H1 (:,1) {mustBePositive} = 10 % typical value, = ?
-        options.P_H1 (:,1) {mustBeNonnegative} = 0.8
+        P_smooth_past (:,1) = 0.5
+        options.alpha (1,1) = 0.8 % according to literature
+        options.SNR_H1 (:,1) {mustBePositive} = 31.62 % typical value = 31.62
+        options.P_H1 (:,1) {mustBeNonnegative} = 0.5
         %options.P_H0 (1,1) === 1 - options.P_H1
             % NOTE: not in dB
     end
@@ -24,10 +25,19 @@ function [sigma_N2,GLR] = noise_track(Yl,sigma_N2_past,options)
     p_Y_on_H1 = 1./(sigma_N2_past*pi.*(1+options.SNR_H1)).*...
         exp(-abs(Yl).^2./(sigma_N2_past.*(1+options.SNR_H1)));
     
+    mu = 1./(1+options.SNR_H1).*...
+        exp(options.SNR_H1./(1+options.SNR_H1).*abs(Yl).^2./sigma_N2_past);
+    % to avoid DIV/0
+    
     P_H1 = options.P_H1;
     P_H0 = 1 - P_H1;
     
-    P_H1_post = P_H1.*p_Y_on_H1./(P_H1.*p_Y_on_H1+P_H0.*p_Y_on_H0);
+    P_H1_post = 1-P_H0./(P_H1.*mu+P_H0);
+    
+    P_smooth = 0.9*P_smooth_past+0.1*P_H1_post;
+    
+    stagged_idx = P_smooth>=0.99;
+    P_H1_post(stagged_idx) = min(0.99,P_H1_post(stagged_idx));
     
     P_H0_post = 1-P_H1_post;
     
@@ -36,7 +46,7 @@ function [sigma_N2,GLR] = noise_track(Yl,sigma_N2_past,options)
     
     sigma_N2 = options.alpha*sigma_N2_past+(1-options.alpha)*E_cond; 
     
-    GLR = P_H1.*p_Y_on_H1./P_H0./p_Y_on_H0;
+    GLR = P_H1./P_H0.*mu;
 
 
 end
